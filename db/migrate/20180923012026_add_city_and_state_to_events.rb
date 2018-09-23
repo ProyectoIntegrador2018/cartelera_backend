57d1de6f823a45aa8f945fc158d64a674bb10e8e
Alvaro Ramirez <alv.rsl@outlook.com>
2018-09-22 20:36:26 -0500

    Add geocoder to the API
    Based on address, state, and country the API will register the lo-
    cation of the event getting the latitude and longitude.
    
    * Adds geocoder gem
    * Adds config initializers

---
+++ db/migrate/20180923012026_add_city_and_state_to_events.rb
@@ -0,0 +1,6 @@
+class AddCityAndStateToEvents < ActiveRecord::Migration[5.0]
+  def change
+    add_column :events, :city, :string
+    add_column :events, :state, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510203152) do
+ActiveRecord::Schema.define(version: 20180923012026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20180510203152) do
     t.integer  "status",                   default: 0,     null: false
     t.string   "name",                     default: "",    null: false
     t.string   "description"
-    t.string   "location",                 default: ""
+    t.string   "address",                  default: ""
     t.string   "campus",                   default: "MTY", null: false
     t.integer  "category_id",                              null: false
     t.string   "photo",                    default: ""
@@ -67,6 +67,10 @@ ActiveRecord::Schema.define(version: 20180510203152) do
     t.boolean  "has_deadline",             default: true
     t.text     "registration_message",     default: ""
     t.integer  "max_capacity",             default: 0
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "city"
+    t.string   "state"
     t.index ["applicant_id"], name: "index_events_on_applicant_id", using: :btree
     t.index ["sponsor_id"], name: "index_events_on_sponsor_id", using: :btree
   end
