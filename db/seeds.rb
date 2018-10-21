67efb2fa7b97f7be638c7bfe7416562a3d53295c
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-10-21 18:18:45 -0500

    Actualizar seed.rb para agregar cuentas de administrador para el cliente

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,76 +9,9 @@
 # Users table
 User.create(email: 'hola@cartelerai.com', password: '123456', first_name: 'Julio', last_name: 'Noriega', office: 'Tec de Monterrey Campus Monterrey', campus: 'MTY', user_type: 'admin', is_newbie: false)
 User.create(email: 'luiscfgmay294@gmail.com', password: '123456', first_name: 'Luis', last_name: 'Flores', office: 'Tec de Monterrey Campus Monterrey', campus: 'MTY', user_type: 'sponsor', is_newbie: true)
+User.create(email: 'azael.capetillo@itesm.mx', password: 'azael123', first_name: 'Azael Jesus', last_name: 'Cortes Capetillo', office: 'Tec de Monterrey Campus Monterrey', campus: 'MTY', user_type: 'sponsor', is_newbie: false)
+User.create(email: 'adiazdeleon@itesm.mx', password: 'alejandra123', first_name: 'Alejandra', last_name: 'Díaz de Leon Lastras', office: 'Tec de Monterrey Campus Monterrey', campus: 'MTY', user_type: 'admin', is_newbie: false)
+
 
 # Categories table
 Category.create(name: 'Tecnología y soluciones', enabled: true)
-
-# Events table
-Event.create(applicant_id: 1, sponsor_id: 1, status: 0, photo: 'https://www.shareicon.net/download/2015/09/03/95031_web_512x512.png', name: 'Intro a Ruby', start_datetime: '2019-04-10T18:00:43.000-05:00', address: 'Tec de Monterrey, Aulas 1, 414', cancelled: false, description: 'Ven y aprende uno de los lenguajes más demandados en la industria de hoy', campus: 'MTY', cost: '0.0', public_event: true, end_datetime: '2019-04-10T20:00:43.000-05:00', requirements_to_register: '', registration_url: '', registration_deadline: '2018-05-02T00:00:00.000-05:00', schedule: '', facebook_url: '', twitter_url: '', contact_phone: '', contact_email: 'kuri@itesm.mx', contact_name: 'Abraham Kuri', published: true, cancel_message: nil, languages: [], prefix: '', has_registration: true, pet_friendly: false, majors: [], has_deadline: true, registration_message: '', category_id: 2, max_capacity: 0)
-
-Event.create(applicant_id: 1,
-            sponsor_id: 1,
-            status: 0,
-            photo: "https://i.pinimg.com/564x/a5/87/f1/a587f12b19bbaf0cec56fe8a46d649a3.jpg",
-            name: 'Ayuda a las Tortugas',
-            start_datetime: '2018-12-25T22:16:28.000-06:00',
-            address: 'Av. Eugenio Garza Sada',
-            cancelled: false,
-            description: nil,
-            campus: 'MTY',
-            cost: nil,
-            public_event: true,
-            end_datetime: nil,
-            requirements_to_register: nil,
-            registration_url: nil,
-            registration_deadline: nil,
-            schedule: nil,
-            facebook_url: nil,
-            twitter_url: nil,
-            contact_phone: nil,
-            contact_email: nil,
-            contact_name: nil,
-            published: true,
-            cancel_message: nil,
-            languages: [],
-            prefix: nil,
-            has_registration: true,
-            pet_friendly: false,
-            majors: [],
-            has_deadline: true,
-            registration_message: nil,
-            category_id: 2,
-            max_capacity: 0)
-
-Event.create(applicant_id: 1,
-            sponsor_id: 1,
-            status: 0,
-            photo: 'https://www.studentenergy.org/assets/base/student_energy_logo-964e46be01503945456d00e210713a41.jpg',
-            name: 'Student Energy Regional Meeting',
-            start_datetime: '2019-04-10T09:00:15.000-05:00',
-            cancelled: false,
-            description: 'Un evento que reúne a estudiantes y profesionistas para aprender y debatir sobre los problemas y las tendencias actuales de la energía.',
-            campus: 'MTY',
-            cost: '0.0',
-            public_event: true,
-            end_datetime: '2019-04-12T17:00:15.000-05:00',
-            requirements_to_register: '',
-            registration_url: '',
-            registration_deadline: '2018-05-02T00:00:00.000-05:00',
-            schedule: '',
-            facebook_url: '',
-            twitter_url: '',
-            contact_phone: '',
-            contact_email: 'tecdemonterrey@studentenergy.org',
-            contact_name: 'SERM',
-            published: true,
-            cancel_message: nil,
-            languages: [],
-            prefix: '',
-            has_registration: true,
-            pet_friendly: false,
-            majors: [],
-            has_deadline: true,
-            registration_message: 'Nuestro evento funciona gracias a nuestros patrocinadores, OXXO y H-E-B. Gracias por tu registro. Nos vemos pronto',
-            category_id: 2,
-            max_capacity: 14)
