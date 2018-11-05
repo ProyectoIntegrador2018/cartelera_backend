79d218fb2713efc486c3f9ef3994bcfdf9ea9667
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-11-05 09:35:52 -0600

    Add status to event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,13 +2,13 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :photo, :name, :start_datetime, :address,
              :cancelled, :description, :campus, :category, :category_name, :cost,
-             :public_event, :end_datetime, :requirements_to_register,
+             :public_event, :end_datetime, :full_address, :requirements_to_register,
              :registration_url, :registration_deadline, :schedule,
              :facebook_url, :twitter_url, :contact_phone, :contact_email,
              :contact_name, :published, :cancel_message, :languages, :prefix,
              :has_registration, :pet_friendly, :majors, :has_deadline,
              :registration_message, :tag_names, :max_capacity, :category_id,
-             :registered_count, :latitude, :longitude, :city, :state
+             :registered_count, :latitude, :longitude, :city, :state, :status
 
   def category
     return 'General' unless object.category_name
