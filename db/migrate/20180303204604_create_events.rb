785384cfb4aedfc73547456bbb352a81085c810b
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-11-05 09:34:42 -0600

    Add reviwe_comments attribute to event

--- db/migrate/20180303204604_create_events.rb
+++ db/migrate/20180303204604_create_events.rb
@@ -9,7 +9,7 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.string :address
       t.string :campus, null: false
       t.integer :category_id, null: false
-      t.string :photo
+      t.string :photo, default: 'http://gda.itesm.mx/zonaei/content/bqsbCK0XJW/logo-iag_LCB.png'
       t.decimal :cost
       t.boolean :public, default: true, null: false
       t.datetime :start_datetime
