10ded76a444491515a5b7c6f783bd460cb314ffd
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-11-08 11:32:58 -0600

    Show applicant's upcoming and past events

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
       get '/upcoming_events' => 'events#upcoming_events'
       get 'events/:id/registrees' => 'events#registrees'
       get 'applicant_events/:applicant_id' => 'events#applicant'
+      get 'applicant/:applicant_id/upcoming_events' => 'events#upcoming_applicant'
+      get 'applicant/:applicant_id/past_events' => 'events#past_applicant'
 
       # Registry routes
       resources :registrees, only: %i[index show create destroy]
