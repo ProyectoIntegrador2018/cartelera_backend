2d59e5af9160bfa18f814a06d8698854686e13d4
Luis Carlos Flores <luiscfgmay294@gmail.com>
2018-11-10 18:23:02 -0600

    Add applicant events to sponsor list

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -39,7 +39,7 @@ class Api::V1::EventsController < ApplicationController
 
   def upcoming
     @events = Event.upcoming
-    @events = @events.where(sponsor: current_user) unless current_user.admin?
+    @events = @events.where(sponsor: current_user || sponsor_id == current_user.sponsored_by) unless current_user.admin?
     render json: event_json, status: 200
   end
 
@@ -57,7 +57,7 @@ class Api::V1::EventsController < ApplicationController
 
   def past
     @events = Event.past
-    @events = @events.where(sponsor: current_user) unless current_user.admin?
+    @events = @events.where(sponsor: current_user || sponsor_id == current_user.sponsored_by) unless current_user.admin?
     render json: event_json, status: 200
   end
 
